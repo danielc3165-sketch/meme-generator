2b1db3281148aec736557237c8d6412e9f661315
danielc3165-sketch <daniel.c3165@gmail.com>
2026-06-09 01:39:17 +0300

    canvas inputs and download

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 'use strict'
 
-console.log('Hello, World! 1') 
+function onChangeText(elValue) {
+    const canvas = document.querySelector('.canvas');
+    const ctx = canvas.getContext('2d');
+    var color = document.querySelector('.color-input').value;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = color;
+    ctx.font = '30px Arial';
+    ctx.fillText(elValue, 10, 50);
+  //console.log(elValue);
+}
+
+function downloadMeme(link){
+    const canvas = document.querySelector('.canvas');
+    link.href = canvas.toDataURL();
+}
